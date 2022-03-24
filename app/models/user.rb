12fc092c5246cfc9ffadaffe4132cdca06c120d6
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-03-24 13:01:44 +0900

    バリデーション追加

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  validates :nickname,           presence: true 
+  validates :familyname,         presence: true
+  validates :firstname,          presence: true
+  validates :familyname_katakana,presence: true
+  validates :firstname_katakana, presence: true
+  validates :birthday,           presence: true
+end
