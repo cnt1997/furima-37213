ec3b9ed661e07bd250508d31ddacf981efdcc2b9
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-03-28 17:34:47 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  VALID_NAME = /\A[ぁ-んァ-ン一-龥]/
+  VALID_KATAKANA = /\A[ァ-ヶー－]+\z/
 
-  validates :password,           presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/}
+  validates :password,           presence: true, format: {with: VALID_PASSWORD}
   validates :nickname,           presence: true 
-  validates :familyname,         presence: true
-  validates :firstname,          presence: true
-  validates :familyname_katakana,presence: true
-  validates :firstname_katakana, presence: true
+  validates :familyname,         presence: true, format: {with: VALID_NAME} 
+  validates :firstname,          presence: true, format: {with: VALID_NAME}
+  validates :familyname_katakana,presence: true, format: {with: VALID_KATAKANA}
+  validates :firstname_katakana, presence: true, format: {with: VALID_KATAKANA}
   validates :birthday,           presence: true
 end
