4147d3f44c1dfec8bb9acc215628bc0aa07d9b88
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-02 00:40:14 +0900

    active_hashのデータ記述

---
+++ app/models/days_up_to_delivery.rb
@@ -0,0 +1,10 @@
+class Days_up_to_delivery < ActiveHash::Base
+  self.data = [
+    {id: 1, name: '---'},
+    {id: 2, name: '1~2日で発送'},
+    {id: 3, name: '2~3日で発送'},
+    {id: 4, name: '4~7日で発送'},
+  ]
+  include ActiveHash::Associatons
+  has_many :items
+end
