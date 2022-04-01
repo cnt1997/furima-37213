503eb9dd6fcae22d83af207a07f76b7cc985295f
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-02 00:40:35 +0900

    active_hashバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associatons::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :days_up_to_delivery
+  belongs_to :prefecture
+
   validates :item_name,                 presence: true
   validates :description,               presence: true
-  validates :category_id,               presence: true
-  validates :status_id,                 presence: true
-  validates :delivery_charge_id,        presence: true
-  validates :days_up_to_delivery_id,    presence: true
+  validates :category_id,               presence: true,   numericality: { other_than: 1, message: "can't be blank"}
+  validates :status_id,                 presence: true,   numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_charge_id,        presence: true,   numericality: { other_than: 1, message: "can't be blank"}
+  validates :days_up_to_delivery_id,    presence: true,   numericality: { other_than: 1, message: "can't be blank"}
   validates :price,                     presence: true
-  validates :user,                      presence: true, foreign_key: true
-  validates :prefecture_id,             presence: true
+  validates :user,                      presence: true,   foreign_key: true
+  validates :prefecture_id,             presence: true,   numericality: { other_than: 1, message: "can't be blank"}
 
   belongs_to :user
   has_one_attached :image
