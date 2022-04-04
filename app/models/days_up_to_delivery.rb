065f400b84e6a9c1ffe369c4314405eb280d787e
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-04 19:39:57 +0900

    ActiveHash記述修正

--- app/models/days_up_to_delivery.rb
+++ app/models/days_up_to_delivery.rb
@@ -1,10 +1,10 @@
-class Days_up_to_delivery < ActiveHash::Base
+class DaysUpToDelivery < ActiveHash::Base
   self.data = [
     {id: 1, name: '---'},
     {id: 2, name: '1~2日で発送'},
     {id: 3, name: '2~3日で発送'},
     {id: 4, name: '4~7日で発送'},
   ]
-  include ActiveHash::Associatons
+  include ActiveHash::Associations
   has_many :items
 end
--- app/models/delivery_charge.rb
+++ app/models/delivery_charge.rb
@@ -1,9 +1,9 @@
-class Delivery_change < ActiveHash::Base
+class DeliveryCharge < ActiveHash::Base
   self.data = [
     {id: 1, name: '---'},
     {id: 2, name: '着払い（購入者負担）'},
     {id: 3, name: '送料込み（出品者負担）'},
   ]
-  include ActiveHash::Associatons
+  include ActiveHash::Associations
   has_many :items
 end
