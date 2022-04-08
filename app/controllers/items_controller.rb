9d25add56661e32da0a2f5ffae2a9a972557eb27
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-08 16:44:25 +0900

    商品投稿ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,14 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def item_params
-    params.require(:item).permit()
+    params.require(:item).permit(:item_name, :description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_up_to_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
