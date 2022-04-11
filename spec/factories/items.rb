4cc4bc36dcfea6836b020e909de13d5a76d6844a
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-11 13:46:36 +0900

    修正1

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,6 @@ FactoryBot.define do
     delivery_charge_id  {Faker::Number.between(from: 2, to: 3)}
     days_up_to_delivery_id  {Faker::Number.between(from: 2, to: 4)}
     price               {Faker::Number.between(from: 300, to: 9999999)}
-    #user_id             {1}
     prefecture_id       {Faker::Number.between(from: 2, to: 48)}
 
     association :user
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,6 +74,13 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Days up to delivery can't be blank")
       end
 
+      it "priceが空では登録できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+
       it "priceが300未満では登録できない" do
         @item.price = 10
         @item.valid?
@@ -102,6 +109,18 @@ RSpec.describe Item, type: :model do
         @item.prefecture_id = "1"
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it "imageが空では登録できない" do 
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it "userが紐づいていなければ出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
