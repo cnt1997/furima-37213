4489819d7e8e51520e9e1aecf3846bf853cceeae
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-21 21:42:40 +0900

    コントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,9 +16,21 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+
 
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_up_to_delivery_id, :price, :image).merge(user_id: current_user.id)
