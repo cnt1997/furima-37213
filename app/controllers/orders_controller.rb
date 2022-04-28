33112c31ce75a1422e136335ef562487ecd1e7e8
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-28 23:04:04 +0900

    orderコントローラー記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,24 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order_address = DonationAddress.new(order_params)
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
   def order_params
-    params.require(:order_params).permit(postal_code: postal_code, prefecture_id: prefecture_id, city: city,zblock: block,
-      building: building, phone_number: phone_number, order_id: order.id, item_id: item_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, 
+      :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
   
 end
