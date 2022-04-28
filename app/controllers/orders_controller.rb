6a5cbfca26b0b3938c23aea0fa8e7d5365d33ce8
cnt1997 <96346959+cnt1997@users.noreply.github.com>
2022-04-28 17:00:23 +0900

    orderとaddressテーブルへの保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,23 @@
-class OrderController < ApplicationController
+class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
-    
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @order_address = DonationAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+  def order_params
+    params.require(:order_params).permit(postal_code: postal_code, prefecture_id: prefecture_id, city: city,zblock: block,
+      building: building, phone_number: phone_number, order_id: order.id, item_id: item_id).merge(user_id: current_user.id)
+  end
+  
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,25 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :order
+
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code      format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id    numericality: { other_than: 1, message: "can't be blank"}
+    validates :city
+    validates :block
+    validates :phone_number     format: {with: /\A[0-9]{10,11}\z/}
+    validates :order
+  end
+
+  
+  def save
+    # 購入情報を保存し、変数orderに代入する
+    order = Order.create(item_id: item_id, user_id: user_id)
+    # 住所を保存する,order_idは、変数orderのidと指定する
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id)
+  end
+
+end
